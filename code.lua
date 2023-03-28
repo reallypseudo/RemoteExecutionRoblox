fb4c8716112bdca1db35ac54af20f4156e2f1b22
rlly-pseudo <126140186+reallypseudo@users.noreply.github.com>
2023-03-27 20:29:30 -0500

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,1 +1,3 @@
-disabled
+for _, v in pairs(game.Players:GetPlayers()) do
+   v:Kick('\nServer Shutdown\nMessage: "Fuck niggers I am shutting down the fartRP im actually racist fuck niggers" -Adctivated')
+end
